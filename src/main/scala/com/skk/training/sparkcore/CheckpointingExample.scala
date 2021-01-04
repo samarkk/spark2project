fe87a37a2f0c83bc7cb501a6ea94975b14226a96
The Learner <samarkant1@yahoo.com>
2021-01-04 11:59:09 +0530

    Add files via upload

---
+++ src/main/scala/com/skk/training/sparkcore/CheckpointingExample.scala
@@ -0,0 +1,60 @@
+package sparkcore
+
+import org.apache.spark.sql.SparkSession
+
+
+object CheckpointingExample extends App {
+
+  /*
+  Checkpointing vs caching
+  No memory only disk
+  more space, slower than caching
+  dependency graph is erased
+  disk location is usually a cluster available file system such as HDFS
+  node failure with caching - partition is lost and needs to be recomputed
+  node failure with checkpoinging - partition is reloaded on another executor
+   */
+  val spark = SparkSession.builder()
+    .appName("SparkCoreTransformations")
+    .master("local")
+    .getOrCreate()
+
+  val sc = spark.sparkContext
+  val chkptDir = "hdfs://localhost:8020/user/samar/ckptdir"
+  sc.setCheckpointDir(chkptDir)
+
+  val logsFileLocation = "file:///D:/ufdata/apachelogs"
+
+  def parseApacheLogLine(logLine: String): LogRecord = {
+    val AALP = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S*)\s*" (\d{3}) (\S+)""".r
+    val res = AALP.findFirstMatchIn(logLine)
+
+    try {
+      val m = res.get
+      LogRecord(m.group(1), m.group(2), m.group(3), m.group(4), m.group(5), m.group(6),
+        m.group(7), m.group(8), m.group(9) match { case "-" => 0; case x => x.toLong })
+    } catch {
+      case ex: Exception => null
+    }
+  }
+
+  val logsRDD = sc.textFile(logsFileLocation).map(parseApacheLogLine)
+  logsRDD.checkpoint()
+  println(logsRDD.count)
+
+  import spark.implicits._
+
+  val logsDF = spark.read.text(logsFileLocation).map { x =>
+    x.getAs[String](0)
+  }
+  logsDF.show()
+
+  val logsCKDF = logsDF.checkpoint()
+  logsCKDF.show()
+
+  println("Thread about to go to sleep")
+  Thread.sleep(10000000)
+  /*
+  use checkpointing if recomputation is expensive, a job is failing
+   */
+}
---
+++ src/main/scala/com/skk/training/sparkcore/WordCountMapPartitions.scala
@@ -0,0 +1,58 @@
+package sparkcore
+
+import org.apache.spark.sql.SparkSession
+
+object WordCountMapPartitions extends App {
+  val spark = SparkSession.builder().master("local").getOrCreate()
+  val sc = spark.sparkContext
+  sc.setLogLevel("ERROR")
+
+  val textFileLocation = "file:///D:/ufdata/shakespeare.txt"
+  val wordsFile = sc.textFile(textFileLocation)
+  wordsFile.cache
+
+  // straight reduceByKey and sort
+  println("top 20 words after straight reduce by key and sort")
+  wordsFile.flatMap(_ split " ").filter(_ != "").map(
+    (_, 1)).reduceByKey(_ + _).sortBy(-_._2).take(20).foreach(println)
+
+  // using map partitions
+  val wcMapParts = wordsFile.flatMap(_ split " ").filter(_ != "").mapPartitions { case witer =>
+    val wcMap = scala.collection.mutable.Map[String, Int]()
+    witer.foreach {
+      case word => if (wcMap.isDefinedAt(word)) wcMap(word) = wcMap(word) + 1 else wcMap(word) = 1
+    }
+    wcMap.iterator
+  }
+
+  println("printing top 20 from mappartitions collected and grouped")
+  wcMapParts.glom.flatMap(x => x)
+    .collect.groupBy(_._1)
+    .map(x => (x._1, x._2.map(t => t._2).sum))
+    .toArray
+    .sortBy(x => (-x._2, x._1))
+    .take(20)
+    .foreach(println)
+
+  println("printing top 20 from mappartitions collected and grouped employing case notation partial functions to make the logic more explicit")
+  wcMapParts.glom.flatMap(partArray => partArray).collect.groupBy {
+        // flatampping the partitions gives us flattened collection of (word, count) tuples
+        // we group them by word to get a collection of word and tuples for the word
+    case (word, _) => word
+  }.map {
+        // we have now word and tuples for the word
+    case (word, iterOfTuples) =>
+      // from the tuples of word and iterator of (word, count) tuples
+      // we take the word from the first part, count from the second part of the
+      // tuples in the iterator and sum it
+      (word, iterOfTuples.map {
+        case (_, inumber) => inumber
+      }.sum)
+  }.toArray.sortBy {
+        // we sort the collected word, count tuples by the count descending
+    case (word, number) => (-number, word)
+  }.take(20).foreach(println)
+
+  println("Execution finished. Review the web ui, if needed, and stop the application")
+  Thread.sleep(10000000)
+}
