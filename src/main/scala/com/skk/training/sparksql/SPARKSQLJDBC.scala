f695575e3465150813c16963d8ec0142da9208bf
The Learner <samarkant1@yahoo.com>
2023-01-28 00:43:56 +0530

    Update SPARKSQLJDBC.scala

--- src/main/scala/com/skk/training/sparksql/SPARKSQLJDBC.scala
+++ src/main/scala/com/skk/training/sparksql/SPARKSQLJDBC.scala
@@ -1,25 +1,61 @@
 package com.skk.training.sparksql
-import org.apache.spark.{ SparkConf, SparkContext }
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SQLContext
 import java.util.Properties
-import org.apache.spark.sql.SaveMode
 
 object SPARKSQLJDBC extends App {
-  val sc = new SparkContext(new SparkConf().setMaster("local[*]").setAppName("Spark_SQL_App"))
-  sc.setLogLevel("WARN")
-  val sqlc = new SQLContext(sc)
-  import sqlc.implicits._
+  val spark = SparkSession.
+      builder().
+      appName("SparkJDBC").
+      getOrCreate()
 
   val prop = new Properties()
-  prop.put("user", "root"); prop.put("password", "cloudera");
-  prop.put("driver", "com.mysql.jdbc.Driver")
-  val mrdf = sqlc.read.jdbc("jdbc:mysql://localhost:3306/testdb", "simptable", prop)
-  mrdf.collect.foreach(println)
-  val mrdf_f = sqlc.read.jdbc("jdbc:mysql://localhost:3306/testdb", "simptable",
-    "age", 0, 60, 2, prop)
-  print(mrdf_f.collect)
-  mrdf.filter(mrdf("age") > 30).show
-  mrdf.filter(mrdf("age") > 30 && mrdf("fname").startsWith("a")).select("fname").show
-  //  mrdf.write.jdbc("jdbc:mysql://localhost:3306/testdb", "nstable", prop)
-  mrdf.write.mode(SaveMode.Append).jdbc("jdbc:mysql://localhost:3306/testdb", "nstable", prop)
+prop.put("user", "root"); prop.put("password", "ramanShastri24!");
+prop.put("driver", "com.mysql.jdbc.Driver")
+
+val url = "jdbc:mysql://localhost:3306/testdb"
+val password = "ramanShastri24!"
+val mockdf = spark.read.format("jdbc").
+option("url", url).
+option("dbtable", "mocktbl") .
+option("user", "root").
+option("password", password ) .
+option("driver", "com.mysql.cj.jdbc.Driver").load()
+
+mockdf.show()
+mockdf.printSchema()
+mockdf.rdd.glom().map(_.length).collect()
+
+val mockdf_part = spark.read.format("jdbc").
+  option("url", url).
+  option("dbtable", "mocktbl") .
+  option("user", "root").
+  option("password", password).
+  option("driver", "com.mysql.cj.jdbc.Driver").
+  option("partitionColumn", "id").option("lowerBound", 0).
+  option("upperBound", 1000).
+  option("numPartitions", 4).load()
+
+mockdf_part.show()
+mockdf.rdd.glom().map(_.length).collect()
+
+// the partition stride is (upberBound - lowerBound)/numPartitions
+spark.read.format("jdbc").
+option("url", url).
+option("dbtable", "mocktbl") .
+option("user", "root").
+option("password","ramanShastri24!") .
+option("driver", "com.mysql.jdbc.Driver") .option("partitionColumn", "id").
+option("lowerBound", 950).
+option("upperBound", 1000) .
+option("numPartitions", 4).load().rdd.glom().map(_.length).collect()
+
+val filter_query = "(select fname, lname from mocktbl where id between 10 and 20) fq"
+val pushDownDF= spark.read.jdbc("jdbc:mysql://localhost:3306/testdb", filter_query , prop)
+
+// predicate pushdown works only with data frames, not with sql
+pushDownDF.explain()
+mockdf.filter("id between 10 and 20").explain()
+
+mockdf.write.mode("append").jdbc("jdbc:mysql://localhost:3306/testdb","mocktblcp",prop)
 }
